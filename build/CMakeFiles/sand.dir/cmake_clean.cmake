25e85e6cd64ab63ce0b0fd8af04feaa27337a73b
CastSo <scasttr003@gmail.com>
2025-10-14 22:47:58 -0700

    initial commit

---
+++ build/CMakeFiles/sand.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sand.dir/src/config.cpp.o"
+  "CMakeFiles/sand.dir/src/config.cpp.o.d"
+  "CMakeFiles/sand.dir/src/glad.c.o"
+  "CMakeFiles/sand.dir/src/glad.c.o.d"
+  "CMakeFiles/sand.dir/src/main.cpp.o"
+  "CMakeFiles/sand.dir/src/main.cpp.o.d"
+  "CMakeFiles/sand.dir/src/shader.cpp.o"
+  "CMakeFiles/sand.dir/src/shader.cpp.o.d"
+  "sand"
+  "sand.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/sand.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
